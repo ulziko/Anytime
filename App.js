ce50ec57a60b96c6247b0ebc05d4993fab1f333f
=Nominnna <=>
2024-07-31 12:10:42 +0800

    commented

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import AppNavigation from './navigation/appNavigation';
 import {UserStore} from './context/UserContext';
 
 export default function App() {
+  // nomioo check
   return (
     <UserStore>
       <AppNavigation />
