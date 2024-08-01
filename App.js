2b4059f57c60a1485aa4c5c7e2c18fce5816c7eb
v10d <v10d@tool.v10d.tmp>
2024-08-01 09:54:57 +0800

    App.js edited

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import AppNavigation from './navigation/appNavigation';
 import {UserStore} from './context/UserContext';
-import Home from './screens/v10d/HomeScreen';
 
 export default function App() {
     return (
